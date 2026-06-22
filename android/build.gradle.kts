5c9e5d3dd0e2f31ec6632041a93b687fd645e949
Alex Pinilla <alexpinilla95@gmail.com>
2026-06-22 09:05:43 +0200

    release 4.0.6 - update native iOS and Android SDKs to 4.0.6 (iOS App Store privacy-manifest fix ITMS-91061, Android PACE NFC, KYB company & key people, loader/whitelabel fixes; fix iOS verification UI opening twice)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 group = "me.didit.sdk.sdk_flutter"
 version = "1.0-SNAPSHOT"
 
-val diditSdkAndroidVersion = "4.0.5"
+val diditSdkAndroidVersion = "4.0.6"
 val diditSdkAndroidVariant = (
     rootProject.findProperty("diditSdkAndroidVariant")
         ?: findProperty("diditSdkAndroidVariant")
