00ac68cf422d1500a8adf4425bef9497b02ffea1
Alejandro Rosas <alejandrorosas1997@gmail.com>
2026-09-14 19:02:37 +0200

    Fix iOS configuration argument order

--- packages/didit_sdk_nfc/ios/didit_sdk_nfc/Sources/didit_sdk_nfc/SdkFlutterPlugin.swift
+++ packages/didit_sdk_nfc/ios/didit_sdk_nfc/Sources/didit_sdk_nfc/SdkFlutterPlugin.swift
@@ -198,7 +198,6 @@ public class SdkFlutterPlugin: NSObject, FlutterPlugin {
 
         return DiditSdk.Configuration(
             languageLocale: language,
-            showLanguageSelector: dict["showLanguageSelector"] as? Bool ?? false,
             fontFamily: dict["fontFamily"] as? String,
             loggingEnabled: dict["loggingEnabled"] as? Bool ?? false,
             showCloseButton: dict["showCloseButton"] as? Bool ?? true,
@@ -207,7 +206,8 @@ public class SdkFlutterPlugin: NSObject, FlutterPlugin {
             defaultDocumentCamera: defaultDocumentCamera,
             defaultLivenessCamera: defaultLivenessCamera,
             showDocumentCameraSwitchButton: dict["showDocumentCameraSwitchButton"] as? Bool ?? true,
-            showLivenessCameraSwitchButton: dict["showLivenessCameraSwitchButton"] as? Bool ?? true
+            showLivenessCameraSwitchButton: dict["showLivenessCameraSwitchButton"] as? Bool ?? true,
+            showLanguageSelector: dict["showLanguageSelector"] as? Bool ?? false
         )
     }
 
